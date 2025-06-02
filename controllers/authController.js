cd9aebb63aca229c3c926f28cc173d59b5de7f3e
mahesh2291 <gmahesh252525@gmail.com>
2025-06-02 14:30:38 -0400

    authRouter

---
+++ controllers/authController.js
@@ -0,0 +1,88 @@
+const User=require('../models/User')
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+
+const generateToken=(userId)=>{
+    return jwt.sign({id:userId},process.env.JWT_SECRET,{expiresIn:'7d'})
+}
+
+const registerUser=async(req,res)=>{
+    try {
+        const {name,email,password}=req.body
+
+        const userExist=await User.findOne({email})
+        if(userExist) {
+            return res.status(400).json({message:"User already exist"})
+        }
+
+        const hashedPassword=await bcrypt.hash(password,10)
+
+        const user=await User.create({
+            name:name,
+            email:email,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id)
+        })
+    } catch (error) {
+        res.status(500).json({message:"Server error",error:error.message})
+    }
+
+}
+
+
+const loginUser=async(req,res)=>{
+
+    try {
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+        if(!user) {
+            return res.status(500).json({message:"No User Found"})
+        }
+
+        const isPasswordValid=bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid) {
+            return res.status(500).json({message:'Invalid Email or Password'})
+        }
+
+        return res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id)
+        })
+
+    } catch (error) {
+        res.status(500).json({message:"Server error",error:error.message})
+    }
+
+    
+
+   
+}
+
+const getUserProfile=async(req,res)=>{
+    try {
+        const {id}=req.body
+        const user=await User.findById(id).select('-password')
+        if(!user) {
+            return res.status(404).json({message:"No User Found"})
+        }
+
+        res.send({
+            user
+        })
+
+    } catch (error) {
+        res.status(500).json({message:"Server error",error:error.message})
+    }
+}
+
+
+module.exports={registerUser,loginUser,getUserProfile}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express=require('express')
 const cors=require('cors')
 const path=require('path')
 const connectDB = require("./config/db")
+const authRoutes=require('./routes/authRoutes')
 
 const app=express()
 
@@ -18,12 +19,23 @@ app.use(
 
 app.use(express.json())
 
+//Routes
+
+app.use('/api/auth',authRoutes);
+// app.use('/api/sessions',sessionRoutes)
+// app.use('/api/questions',questionRoutes)
+
+// app.use('/api/ai/generate-questions', protect, generateInterviewQuestions)
+// app.use('/api/ai/generate-explaination', protect, generateConceptExplaination)
+
+
+
 app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}))
 
 
 const port=process.env.PORT || 5000
 
-app.listen(()=>{
+app.listen(port,()=>{
     connectDB()
     console.log("server running on port " + port)
 })
